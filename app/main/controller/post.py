a9530f710f0f522b63bd2cc2f08ce3d982ec3cb8
techkamar <techkamar@gmail.com>
2024-07-08 14:59:07 +0530

    Post by username and all posts working

--- app/main/controller/post.py
+++ app/main/controller/post.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter
-from app.main.service.post import create_post
+from app.main.service.post import create_post,get_posts_by_username,get_all_posts
 from app.main.model.post import CreatePost
 
 post_router = APIRouter(prefix='/post')
 
 @post_router.get("s")
 async def get_posts():
-    return []
+    return get_all_posts()
+
+@post_router.get("s/{username}")
+async def get_posts_by_uname(username):
+    return get_posts_by_username(username)
 
 @post_router.post("")
 async def make_post(post:CreatePost):
-    return create_post(post,1)
+    return create_post(post,2)
--- app/main/service/post.py
+++ app/main/service/post.py
@@ -1,5 +1,6 @@
 from app.main.util.database import get_local_session
 from app.main.orm.post import Post
+from app.main.orm.user import User
 
 db_session = get_local_session()
 
@@ -9,3 +10,18 @@ def create_post(postinfo,user_id):
     db_session.commit()
 
     return post.id
+
+def get_posts_by_username(username):
+    posts = []
+    result = db_session.query(Post).join(User).filter(User.username==username).all()
+    for entry in result:
+        posts.append({'id':entry.id,'content':entry.content})
+    return posts
+
+def get_all_posts():
+    posts = []
+    result = db_session.query(Post).all()
+    for entry in result:
+        posts.append({'id':entry.id,'content':entry.content})
+    return posts
+    
