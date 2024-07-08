446f153c0f40b1358dbc60a16fd85b72e5d1f706
techkamar <techkamar@gmail.com>
2024-07-08 16:45:32 +0530

    Create comment working

--- app/main/controller/post.py
+++ app/main/controller/post.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from app.main.service.post import create_post,get_posts_by_username,get_all_posts,like_post_by_id
+from app.main.service.post import create_post,get_posts_by_username,get_all_posts,like_post_by_id, create_post_comment
 from app.main.model.post import CreatePost
 import app.main.util.userinfoutil as UserInfoUtil
 
@@ -23,3 +23,9 @@ async def like_post(request:Request,post_id):
     post_id = int(post_id)
     user_id = UserInfoUtil.get_user_id(request)
     return like_post_by_id(post_id,user_id)
+
+@post_router.post("/{post_id}/comment")
+async def create_comment(request:Request,newPost:CreatePost, post_id):
+    post_id = int(post_id)
+    user_id = UserInfoUtil.get_user_id(request)
+    return create_post_comment(user_id,post_id,newPost)
--- app/main/service/post.py
+++ app/main/service/post.py
@@ -2,6 +2,7 @@ from app.main.util.database import get_local_session
 from app.main.orm.post import Post
 from app.main.orm.user import User
 from app.main.orm.like import Like
+from app.main.orm.comment import Comment
 from sqlalchemy import func
 
 db_session = get_local_session()
@@ -31,3 +32,16 @@ def like_post_by_id(post_id,user_id):
     like = Like(user_id=user_id, post_id=post_id)
     db_session.add(like)
     db_session.commit()
+
+
+def create_post_comment(user_id,post_id,newPost):
+    comment_post = Post(content=newPost.content,user_id=user_id)
+    db_session.add(comment_post)
+    db_session.flush()
+    db_session.refresh(comment_post)
+
+    comment = Comment(user_id=user_id, parent_post_id = post_id, comment_post_id=comment_post.id)
+    db_session.add(comment)
+    db_session.commit()
+
+    return comment_post.id
