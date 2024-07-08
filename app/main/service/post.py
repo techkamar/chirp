150746e87f2ff9c7501fcd757ff2d60adf9a9c10
techkamar <techkamar@gmail.com>
2024-07-08 16:18:27 +0530

    Like count added in post data retrieval

--- app/main/service/post.py
+++ app/main/service/post.py
@@ -2,6 +2,8 @@ from app.main.util.database import get_local_session
 from app.main.orm.post import Post
 from app.main.orm.user import User
 from app.main.orm.like import Like
+from sqlalchemy import func
+
 db_session = get_local_session()
 
 def create_post(postinfo,user_id):
@@ -20,9 +22,9 @@ def get_posts_by_username(username):
 
 def get_all_posts():
     posts = []
-    result = db_session.query(Post).all()
-    for entry in result:
-        posts.append({'id':entry.id,'content':entry.content})
+    result = db_session.query(Post.id,Post.content,Post.created_date,func.count(Like.id)).join(Like,isouter=True).group_by(Post.id).all()
+    for id,content,created_date,likecount in result:
+        posts.append({'id':id,'content':content,'likedby':likecount,'created_date':created_date})
     return posts
     
 def like_post_by_id(post_id,user_id):
