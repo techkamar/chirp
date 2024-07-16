d90bc6f65b813f520e67b34b1ecd59c6857fa4ed
techkamar <techkamar@gmail.com>
2024-07-16 14:34:47 +0530

    Fixed Shebang issue

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-#/usr/bin/bash
+#!/usr/bin/bash
 source ../chirpvenv/bin/activate
 export DB_NAME="chirp"
 export DB_USER="postgres"
-export DB_PASS="chirp"
+export DB_PASS="wt_admin1234"
 export DB_HOST="localhost"
 export DB_PORT="5432"
 uvicorn app.main.main:app --host 0.0.0.0 --port 8080
