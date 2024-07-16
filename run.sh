5f151fed78726a2d7fbf68dabcf6d58d6fe9406c
techkamar <techkamar@gmail.com>
2024-07-16 14:35:23 +0530

    Fixed password issue

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 source ../chirpvenv/bin/activate
 export DB_NAME="chirp"
 export DB_USER="postgres"
-export DB_PASS="wt_admin1234"
+export DB_PASS="chirp"
 export DB_HOST="localhost"
 export DB_PORT="5432"
 uvicorn app.main.main:app --host 0.0.0.0 --port 8080
