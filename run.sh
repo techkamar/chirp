ec98d4e0ae2366b00c9611375cf4a483e8528f12
techkamar <techkamar@gmail.com>
2024-07-17 12:46:00 +0530

    Added JWT secret to ENV variables

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
 source ../chirpvenv/bin/activate
+
+# All ENV variables needed to run
 export DB_NAME="chirp"
 export DB_USER="postgres"
 export DB_PASS="chirp"
 export DB_HOST="localhost"
 export DB_PORT="5432"
+export JWT_SECRET_KEY="my-secret"
+
+
+# Start Server
 uvicorn app.main.main:app --host 0.0.0.0 --port 8080
